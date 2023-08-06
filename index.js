c10b3a7a3b95b7237d90959d1c0eb21e7a91678d
Hassan M. Taurak Jr <mrhassantaurak@gmail.com>
2023-08-06 10:17:19 +0800

    index.js

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
 import { getDatabase, ref, set } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAw1kp981u3pq4PTufyGFE4zbizL_4HzXc",
-  authDomain: "wifastconnect.firebaseapp.com",
-  databaseURL: "https://wifastconnect-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wifastconnect",
-  storageBucket: "wifastconnect.appspot.com",
-  messagingSenderId: "69525369694",
-  appId: "1:69525369694:web:b939b15eba6e83311f55cf"
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-function HelloWord(voucher)
-{
+function HelloWorld(voucher) {
     const db = getDatabase();
-
     const reference = ref(db, 'data/test1');
     
-    set(reference,{
+    set(reference, {
         test: voucher
     });
 
@@ -34,10 +29,14 @@ function HelloWord(voucher)
 
 // Function to extract parameter value from URL
 function getParameterValue(parameterName, url) {
-  var params = new URLSearchParams(new URL(url).search);
-  return params.get(parameterName);
+    var params = new URLSearchParams(new URL(url).search);
+    return params.get(parameterName);
 }
 
-// Get the value of the "voucher" parameter
-var voucherValue = getParameterValue("voucher", url);
-HelloWord(voucherValue);
+// Get the current URL of the page
+var currentUrl = window.location.href;
+
+// Get the value of the "voucher" parameter from the current URL
+var voucherValue = getParameterValue("voucher", currentUrl);
+
+HelloWorld(voucherValue);
